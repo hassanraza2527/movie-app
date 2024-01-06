f0978f82d83fa4ce6d4c18a8780bae5f83d1aa73
hassan2527 <143655316+hassanraza2527@users.noreply.github.com>
2024-01-07 04:23:30 +0500

    h

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -7,11 +7,13 @@ import {
   ScrollView,
   TouchableOpacity,
   View,
+  Text,
 } from 'react-native';
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import { HeartIcon } from 'react-native-heroicons/solid';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles, theme } from '../theme';
+import { LinearGradient } from 'expo-linear-gradient';
 
 var { width, height } = Dimensions.get('window');
 const ios = Platform.OS == 'ios';
@@ -19,6 +21,7 @@ const marginTop = ios ? '' : 'mt-3';
 
 const MovieScreen = () => {
   const { prams: item } = useRoute();
+  let movieName = 'Kung Fu Panda 4';
   const navigation = useNavigation();
   const [isFavorite, toggleFavorite] = useState(false);
   useEffect(() => {}, item);
@@ -67,7 +70,40 @@ const MovieScreen = () => {
             source={require('../assets/images/movie2.png')}
             style={{ width, height: height * 0.55 }}
           />
+          <LinearGradient
+            colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
+            style={{
+              width,
+              height: height * 0.4,
+              position: 'absolute',
+              bottom: 0,
+            }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+          />
         </View>
+      </View>
+      <View style={{ marginTop: -(height * 0.09), paddingVertical: 16 }}>
+        <Text
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 30,
+            fontWeight: 'bold',
+          }}
+        >
+          {movieName}
+        </Text>
+        <Text
+          style={{
+            color: '#6B7280',
+            fontWeight: '600',
+            fontSize: 16,
+            textAlign: 'center',
+          }}
+        >
+          Released . 2024 . 150 min
+        </Text>
       </View>
     </ScrollView>
   );
