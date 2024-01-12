2ec0fb84944e2ddc77a156515f41b56c5ac152b5
hassan2527 <143655316+hassanraza2527@users.noreply.github.com>
2024-01-12 11:14:23 +0500

    api

--- api/moviedb.js
+++ api/moviedb.js
@@ -1,2 +1,8 @@
 import axios from 'axios';
 import { apiKey } from '../constants';
+
+//endpionts
+const apiBase = `https://api.themoviedb.org/3`;
+const trendingMoviesEndpoints = `${apiBase}/trending/movie/day?api_key=${apiKey}`;
+const upComingMoviesEndpoints = `${apiBase}/movie/upcoming?api_key=${apiKey}`;
+const topRatedMoviesEndpoints = `${apiBase}/movie/top_rated?api_key=${apiKey}`;
